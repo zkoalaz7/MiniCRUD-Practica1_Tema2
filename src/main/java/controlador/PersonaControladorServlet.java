1e3b69b59fee823e3f1f1effbe393deda24fd6fe
Lautaro Lopez <zkoalaz246@gmail.com>
2026-09-17 09:14:03 -0300

    Update PersonaControladorServlet.java

--- src/main/java/controlador/PersonaControladorServlet.java
+++ src/main/java/controlador/PersonaControladorServlet.java
@@ -11,7 +11,9 @@ import modelo.vo.PersonaVO;
 @WebServlet("/persona")
 public class PersonaControladorServlet extends HttpServlet {
     private final PersonaDAO dao = new PersonaDAO();
-
+    private double sueldo;
+    private String email;
+    
     // ======================== GET ========================
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -36,6 +38,11 @@ public class PersonaControladorServlet extends HttpServlet {
                 break;
         }
     }
+    public double getSueldo () {
+        return sueldo;
+    }
+    public String getEmail () {
+        return email;
 
     // ======================== POST ========================
     @Override
